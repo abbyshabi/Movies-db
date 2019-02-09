5b983c7b40e790316e852caa53d46e39ac8503de
Oluwadamilola Shabi <abbyshabi@gmail.com>
2019-02-09 22:02:04 +0300

    deleted information

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 export MOVIE_API_KEY='c771c950750147c6bae796ea78707d33'
 export SECRET_KEY='08033063285'
-export MAIL_USERNAME='abbyshabi@gmail.com'
-export MAIL_PASSWORD='oluseyi1'
+
 export FLASK_APP="app.py"
 export FLASK_DEBUG=1
 
